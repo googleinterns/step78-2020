b9603bd3d976780a4892e22d1e14e8371659bada
Maceo Thompson <maceonthompson@gmail.com>
2020-06-22 18:29:22 -0400

    Add Scheduler class (will handle permuting schedules)

---
+++ src/main/java/com/google/sps/Scheduler.java
@@ -0,0 +1,15 @@
+package com.google.sps;
+
+import java.util.Collection;
+
+import com.google.sps.data.*;
+
+/**
+ * Static class for permuting schedules given a list of courses.
+ */
+public final class Scheduler {
+    public Collection<Schedule> generateSchedules(Collection<Course> courses){
+        //TODO: This
+        return null;
+    }
+}
