01010eb3d993ca1c63388ef62d6045279ff0c290
Maceo Thompson <maceonthompson@gmail.com>
2020-06-25 14:10:46 -0400

    Fix indentation

--- src/main/java/com/google/sps/Scheduler.java
+++ src/main/java/com/google/sps/Scheduler.java
@@ -8,8 +8,8 @@ import com.google.sps.data.*;
  * Static class for permuting schedules given a list of courses.
  */
 public final class Scheduler {
-    public List<Schedule> generateSchedules(List<Course> courses, Invariants invariants){
-        //TODO: This
-        return null;
-    }
+  public List<Schedule> generateSchedules(List<Course> courses, Invariants invariants) {
+    // TODO: This
+    return null;
+  }
 }
