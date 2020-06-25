bf3f3291baafdc50e0cad155ed98746d8dd0244b
Maceo Thompson <maceonthompson@gmail.com>
2020-06-25 14:10:13 -0400

    Adjust input/output to take List instead of Collection.

--- src/main/java/com/google/sps/Scheduler.java
+++ src/main/java/com/google/sps/Scheduler.java
@@ -1,6 +1,6 @@
 package com.google.sps;
 
-import java.util.Collection;
+import java.util.List;
 
 import com.google.sps.data.*;
 
@@ -8,7 +8,7 @@ import com.google.sps.data.*;
  * Static class for permuting schedules given a list of courses.
  */
 public final class Scheduler {
-    public Collection<Schedule> generateSchedules(Collection<Course> courses, Invariants invariants){
+    public List<Schedule> generateSchedules(List<Course> courses, Invariants invariants){
         //TODO: This
         return null;
     }
