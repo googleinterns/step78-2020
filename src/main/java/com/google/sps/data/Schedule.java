7acee65dae92420cf6ba485f6f60e98933aa9aab
Maceo Thompson <maceonthompson@gmail.com>
2020-06-26 14:09:12 -0400

    Add proper equals method to Schedule object

--- src/main/java/com/google/sps/data/Schedule.java
+++ src/main/java/com/google/sps/data/Schedule.java
@@ -1,6 +1,7 @@
 package com.google.sps.data;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 public class Schedule {
     private Collection<Course> courses;
@@ -11,6 +12,21 @@ public class Schedule {
         this.weight = -1;
     }
 
+    @Override
+    public boolean equals(Object other) {
+      if (other == this) {
+        return true;
+      }
+
+      if (!(other instanceof Schedule)) {
+        return false;
+      }
+
+      Schedule otherSchedule = (Schedule) other;
+      HashSet<Course> otherCourses = new HashSet<>(otherSchedule.courses);
+      HashSet<Course> thisCourses = new HashSet<>(this.courses);
+      return other.weight == this.weight && otherCourses.equals(thisCourses);
+    }
     //Todo: Add comparator functions based on weight
     //Todo: Add recalculate weight function/set weight function
 }
