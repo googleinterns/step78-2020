272aaa795be62743dbd07f8ecd926637c6b97eb4
Maceo Thompson <maceonthompson@gmail.com>
2020-07-06 11:31:50 -0400

    Fix Scheduler Style and return type

--- src/main/java/com/google/sps/Scheduler.java
+++ src/main/java/com/google/sps/Scheduler.java
@@ -1,15 +1,15 @@
 package com.google.sps;
 
-import java.util.Collection;
-
 import com.google.sps.data.*;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Static class for permuting schedules given a list of courses.
  */
 public final class Scheduler {
-    public Collection<Schedule> generateSchedules(Collection<Course> courses, Invariants invariants){
-        //TODO: This
-        return null;
-    }
+  public List<Schedule> generateSchedules(Collection<Course> courses, Invariants invariants) {
+    // TODO: This
+    return null;
+  }
 }
