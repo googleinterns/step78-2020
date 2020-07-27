098a1fa075df78241388615285627a99d618a8e6
hjohns28 <53411752+hjohns28@users.noreply.github.com>
2020-07-27 13:32:46 -0700

    add parent components for input form  (#41)
    * grandchild components for input form
    
    * add parent components for input form
    
    * delete aria-labels, streamline updateRankSelectOptions()

---
+++ frontend/src/Criterion.js
@@ -0,0 +1,30 @@
+import React, {PureComponent} from 'react';
+import { Card, CardContent } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import SubjectPreference from './SubjectPreference';
+import TimePreference from './TimePreference';
+
+export default class Criterion extends PureComponent {
+  render() {
+    return (
+      <Card>
+        <CardContent>
+          {this.props.times.map((timePreference, index) => (
+            <TimePreference
+              id={index}
+              key={index}
+              timePreference={timePreference}
+              updateTimeStartPreference={this.props.updateTimeStartPreference}
+              updateTimeEndPreference={this.props.updateTimeEndPreference}
+              deleteTimePreference={this.props.deleteTimePreference}
+            />))}
+          <Button onClick={this.props.createNewTimePreference}>+ time-block</Button>
+          <SubjectPreference 
+            subject={this.props.subject}
+            updateSubjectPreference={this.props.updateSubjectPreference}
+          />
+        </CardContent>
+      </Card>
+    )
+  }
+}
