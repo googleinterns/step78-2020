7c2cb5ef13e300f5ac99228986166e6ddee7fcb2
hjohns28 <53411752+hjohns28@users.noreply.github.com>
2020-07-30 11:42:58 -0700

    Integrate FullCalendar into stepper (#52)
    * Adjust schedulr to use Calendar.js and send schedules to calendar
    
    * (Currently Broken) integrate props into calendar.js
    
    * (Broken) Add debugging to calendar.js
    We will end up migrating much of the calendar.js logic to schedulr in order to properly handle a few things.
    
    * Have scheduleList be initialized as a js array, not an object
    
    * lift up state of schedulesTimes from Calendar to Schedulr
    
    * Properly udpate state to pass times to Calendar.js
    
    * delete script.js
    
    * address comments
    
    Co-authored-by: Maceo Thompson <maceonthompson@gmail.com>

--- frontend/src/Schedulr.js
+++ frontend/src/Schedulr.js
@@ -5,6 +5,11 @@ import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import InputForm from './InputForm';
+import Calendar from './Calendar';
+import moment from 'moment';
+
+// the conversion of one minute to 60000 milliseconds
+const MIN_TO_MS = 60000;
 
 class Schedulr extends React.Component {
   constructor(props) {
@@ -12,31 +17,38 @@ class Schedulr extends React.Component {
     this.state = {
       activeStep: 0,
       steps: ['Input Courses and Preferences', 'View Schedules', 'Export to Google Calendar'],
-      scheduleList: {}
+      scheduleList: [],
+      schedulesTimes: [],
     };
 
     this.handleNext = this.handleNext.bind(this);
     this.handleBack = this.handleBack.bind(this);
     this.setScheduleList = this.setScheduleList.bind(this);
+    this.generateScheduleMeetingTimes = this.generateScheduleMeetingTimes.bind(this);
   }
 
   getStepContent() {
-    let step = this.state.activeStep;
+    const step = this.state.activeStep;
     switch (step) {
     case 0:
       return (<InputForm
         handleNext={this.handleNext}
         setScheduleList={this.setScheduleList}/>);
     case 1:
-      return (<p>{JSON.stringify(this.state.scheduleList, null, 2)}</p>);
+      return (<Calendar
+        scheduleList={this.state.scheduleList}
+        schedulesTimes={this.state.schedulesTimes}/>);
     case 2:
-      return 'This is the bit I really care about!';
+      return 'Export to google calendar: coming soon ;)';
     default:
       return 'Unknown step';
     }
   }
 
   handleNext() {
+    if (this.state.activeStep === 0) {
+      this.generateSchedulesTimes();
+    }
     this.setState({...this.state, activeStep: this.state.activeStep + 1});
   }
 
@@ -46,6 +58,56 @@ class Schedulr extends React.Component {
 
   setScheduleList(listOfSchedules) {
     this.setState({...this.state, scheduleList: listOfSchedules});
+  }
+
+  generateSchedulesTimes() {
+    this.state.scheduleList.forEach((schedule) => {
+      this.generateScheduleMeetingTimes(schedule);
+    });
+    return true;
+  }
+
+  generateScheduleMeetingTimes(schedule) {
+    let combinedMeetingTimes = [];
+
+    for (const course of schedule.courses) {
+      // Lecture Sections
+      const lectureTime = this.generateCourseMeetingTimes(course.name, course.lectureSection.meetingTimes);
+      // Lab Sections
+      if (course.labSection) {
+        const labTime = this.generateCourseMeetingTimes(course.name, course.labSection.meetingTimes);
+        combinedMeetingTimes = combinedMeetingTimes.concat(labTime);
+      }
+      combinedMeetingTimes = combinedMeetingTimes.concat(lectureTime);
+    }
+
+    this.setState({
+      ...this.state, schedulesTimes: [...this.state.schedulesTimes, combinedMeetingTimes],
+    });
+  }
+
+  generateCourseMeetingTimes(courseName, meetingTimes) {
+    const lastSunday = this.getLastSunday(Date.now());
+    const courseMeetingTimes = [];
+
+    for (const meetingTime of meetingTimes) {
+      const startTime = new Date(lastSunday.getTime() + meetingTime.start*MIN_TO_MS);
+      const endTime = new Date(startTime.getTime() + meetingTime.duration*MIN_TO_MS);
+
+      courseMeetingTimes.push({
+        title: courseName,
+        start: moment(startTime).format('YYYY-MM-DD HH:mm'),
+        end: moment(endTime).format('YYYY-MM-DD HH:mm'),
+      });
+    }
+    return courseMeetingTimes;
+  }
+
+  getLastSunday(d) {
+    const t = new Date(d);
+    t.setDate(t.getDate() - t.getDay());
+    t.setHours(0, 0, 0, 0);
+    return t;
   }
 
   render() {
